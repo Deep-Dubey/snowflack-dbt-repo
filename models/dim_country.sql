95e3aba05be75c24dcf3311bb831e91490d30530
DEEP DUBEY <deepdubey1995@gmail.com>
2025-11-28 06:35:01 +0000

    seed yaml file added

--- models/dim_country.sql
+++ models/dim_country.sql
@@ -2,3 +2,5 @@ select * from {{ref('code')}}
 -- select * from {{ref('demo_dbt')}}
 -- source: we use source when we get data outside from dbt folder structure
 -- ref: we use when the file present inside dbt folder structure
+-- Every seed go as Table by default
+-- Every model go as View by default
